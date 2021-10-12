baa4ce0abd9b344485385bc118774e38a670a3a0
OlehHnyp <lefk6230@gmail.com>
2021-10-12 08:02:28 +0300

    Added http agent for obtaining data from external api

--- client.py
+++ client.py
@@ -1,71 +1,42 @@
-from twisted.internet import defer
+from twisted.internet import task
 from twisted.internet.protocol import Protocol, ClientFactory
-from twisted.internet import reactor
 
 
 SERVER_HOST = 'localhost'
 SERVER_PORT = 8000
+CURRENCY = 'USDUAH'
+INTERVAL = 5
 
 class CurrencyProtocol(Protocol):
 
-    rates = b''
+    def connectionMade(self):
+        print 'Connection made'
+        self.startLoop()
 
     def dataReceived(self, data):
         print 'data received'
-        self.rates += data
+        print data
 
     def connectionLost(self, reason):
         print 'connection lost', reason
-        self.ratesReceived(self.rates)
 
-    def ratesReceived(self, rates):
-        self.factory.rates_finished(rates)
+    def startLoop(self):
+        self._sender = task.LoopingCall(self.sendRequest)
+        self._sender.start(interval=INTERVAL)
+
+    def sendRequest(self):
+        self.transport.write(CURRENCY)
 
 
 class CurrencyClientFactory(ClientFactory):
 
     protocol = CurrencyProtocol
 
-    def __init__(self, deferred):
-        self.deferred = deferred
-
-    def rates_finished(self, rates):
-        if self.deferred is not None:
-            deferred, self.deferred = self.deferred, None
-            deferred.callback(rates)
-
-    def clientConnectionFailed(self, connector, reason):
-        if self.deferred is not None:
-            deferred, self.deferred = self.deferred, None
-            deferred.errback(reason)
-
-
-def get_rates(host, port):
-    deferred = defer.Deferred()
-    from twisted.internet import reactor
-    factory = CurrencyClientFactory(deferred)
-    reactor.connectTCP(host, port, factory)
-    return deferred
 
 def main():
-
-    @defer.inlineCallbacks
-    def get_currency_rates(host, port):
-        try:
-            rates = yield get_rates(host, port)
-        except Exception as e:
-            print 'rates download failed', e
-            raise
-
-        defer.returnValue(rates)
-
-    def print_rates(rates):
-        print rates
-        print 'done'
-        reactor.stop()
-
-    d = get_currency_rates(SERVER_HOST, SERVER_PORT)
-    d.addCallbacks(print_rates)
+    from twisted.internet import reactor
+    factory = CurrencyClientFactory()
+    reactor.connectTCP(SERVER_HOST, SERVER_PORT, factory)
     reactor.run()
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,29 +1,85 @@
 from twisted.internet.protocol import ServerFactory, Protocol
+from twisted.web.client import Agent, ResponseDone
+from twisted.internet import defer
 
 
 PORT = 8000
 HOST = 'localhost'
+EXTERNAL_SERVER_URL = ''  # add api url here
+
 
 class CurrencyProtocol(Protocol):
 
+    def dataReceived(self, data):
+        print 'received: ', data
+        self.sendData()
+
     def connectionMade(self):
         print 'connection made'
-        self.transport.write('Hello from server')
-        self.transport.loseConnection()
 
     def connectionLost(self, reason):
         print 'connection lost', reason
+
+    def sendData(self):
+        deferred_rates = self.factory.obtainRates()
+        deferred_rates.addCallbacks(self.transport.write, self.sendErrorValue)
+
+    def sendErrorValue(self, failure):
+        self.transport.write(failure.value)
 
 
 class CurrencyServerFactory(ServerFactory):
 
     protocol = CurrencyProtocol
 
+    def __init__(self, agent):
+        self.agent = agent
+
+    def obtainRates(self):
+        return self.agent.performRequest()
+
+
+class HttpAgentHandler(object):
+
+    def __init__(self, agent, url):
+        self.agent = agent
+        self.url = url
+
+    def performRequest(self):
+        response = self.agent.request(b"GET", self.url)
+        return response.addCallback(self.handleResponse)
+
+    def handleResponse(self, response):
+        finished =defer.Deferred()
+        response.deliverBody(HttpBodyHandler(finished))
+        return finished
+
+
+class HttpBodyHandler(Protocol):
+
+    body = b''
+
+    def __init__(self, deferred):
+        self.deferred = deferred
+
+    def dataReceived(self, data):
+        self.body += data
+
+    def connectionLost(self, reason):
+        if reason.check(ResponseDone):
+            self.deferred.callback(self.body)
+        else:
+            self.deferred.errback(reason)
+
+
+
 
 
 def main():
-    factory = CurrencyServerFactory()
     from twisted.internet import reactor
+    agent = Agent(reactor)
+    httpAgentHandler = HttpAgentHandler(agent, EXTERNAL_SERVER_URL)
+    factory = CurrencyServerFactory(httpAgentHandler)
     reactor.listenTCP(port=PORT, interface=HOST, factory=factory)
     reactor.run()
 
