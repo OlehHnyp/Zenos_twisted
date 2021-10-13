bece270447f695e65ed95e6982213ac7e79caec3
OlehHnyp <lefk6230@gmail.com>
2021-10-13 08:38:56 +0300

    Added documentation and comments

--- client.py
+++ client.py
@@ -8,7 +8,7 @@ from twisted.internet.protocol import Protocol, ClientFactory
 
 
 SERVER_HOST = "localhost"
-SERVER_PORT = 8000
+SERVER_PORT = 8123
 INTERVAL = 5  # add interval in seconds for obtaining currency pair rate
 
 class CurrencyProtocol(Protocol):
@@ -24,10 +24,18 @@ class CurrencyProtocol(Protocol):
         print "connection lost", reason
 
     def startLoop(self):
+        """
+        Start sending currency request to the server.
+        :return: None
+        """
         self._sender = task.LoopingCall(self.sendRequest)
         self._sender.start(interval=INTERVAL)
 
     def sendRequest(self):
+        """
+        Send a request to the server with currency inside.
+        :return: None
+        """
         self.transport.write(self.factory.currency)
 
 
@@ -37,7 +45,7 @@ class CurrencyClientFactory(ClientFactory):
 
     def __init__(self, currency):
         self.currency = currency
-        self.file = "clientData/" + "_".join(currency.split("/")) + ".csv"
+        self.file = "clientData/" + "_".join(currency.split("/")) + ".csv"  # file for storing obtained data
         self.fieldnames = [
             "result",
             "base_code",
@@ -51,9 +59,18 @@ class CurrencyClientFactory(ClientFactory):
         ]
 
     def _fileEmptyOrNotExist(self):
+        """
+        Check if the file is empty or not exists.
+        :return: bool
+        """
         return not os.path.isfile(self.file) or os.path.getsize(self.file) == 0
 
     def writeIntoFile(self, data):
+        """
+        Write data into .csv file.
+        :param data: str
+        :return: None
+        """
         data = json.loads(data)
         with open(self.file, "a+") as f:
             writer = csv.DictWriter(f, fieldnames=self.fieldnames)
@@ -64,6 +81,10 @@ class CurrencyClientFactory(ClientFactory):
 
 
 def main():
+    """
+    For each currency create factory and TCP connection, run reactor.
+    :return: None
+    """
     from twisted.internet import reactor
     for currency in sys.argv[1:]:
         factory = CurrencyClientFactory(currency)
--- server.py
+++ server.py
@@ -7,7 +7,7 @@ from twisted.web.client import Agent, ResponseDone
 from twisted.internet import defer
 
 
-PORT = 8000
+PORT = 8123
 HOST = "localhost"
 EXTERNAL_SERVER_URL = ""  # add api url here
 BUFFER_FILE = "serverData/buffer.txt"
@@ -25,10 +25,20 @@ class CurrencyProtocol(Protocol):
         print "connection lost", reason
 
     def sendData(self, data):
+        """
+        Obtain currency rate (or error) and send it to the client.
+        :param data: str
+        :return: None
+        """
         deferredRates = self.factory.obtainRates(data)
         deferredRates.addCallbacks(self.transport.write, self.sendErrorValue)
 
     def sendErrorValue(self, failure):
+        """
+        Send error information to the client.
+        :param failure: Failure
+        :return: None
+        """
         response = dict.fromkeys(self.factory.fieldnames)
         response["error"] = str(failure.value)
         response = json.dumps(response)
@@ -40,9 +50,14 @@ class CurrencyServerFactory(ServerFactory):
     protocol = CurrencyProtocol
 
     def __init__(self, agent, buf):
-        self.agent = agent
-        self.bufferHandler = buf
-        self.bufferData = self.bufferHandler.getBufferData()
+        """
+        Initialize CurrencyServerFactory instance.
+        :param agent: HttpAgentHandler instance
+        :param buf: HttpBodyHandler instance
+        """
+        self.agent = agent  # class instance for handling http requests
+        self.bufferHandler = buf  # class instance for handling buffered data
+        self.bufferData = self.bufferHandler.getBufferData()  # obtain buffered data
         self.fieldnames = [
             "result",
             "base_code",
@@ -57,30 +72,51 @@ class CurrencyServerFactory(ServerFactory):
 
     @defer.inlineCallbacks
     def obtainRates(self, currencies):
-        validRates = self.getValidRates(currencies)
+        """
+        Provide obtaining rates from the buffer or API.
+        :param currencies: str
+        :return: Deferred
+        """
+        validRates = self.getValidRates(currencies)  # check if the buffer stores valid data
         if validRates:
             rates = yield validRates
             print 'got from buffer ', currencies
         else:
-            rates = yield self.agent.performRequest(currencies)
-            rates = yield self.setFields(rates)
+            rates = yield self.agent.performRequest(currencies)  # perform request to API
+            rates = yield self.setFields(rates)  # delete unnecessary fields and add error field
             rates = yield self.addToBufferData(rates, currencies)
             print 'got from API ', currencies
 
         defer.returnValue(rates)
 
     def addToBufferData(self, data, currencies):
+        """
+        Add currency rate to the buffer.
+        :param data: str
+        :param currencies: str
+        :return: str
+        """
         self.bufferData[currencies] = json.loads(data)
         self.bufferHandler.reWriteBufferFile(self.bufferData)
         return data
 
     def getValidRates(self, currencies):
+        """
+        Check if buffer stores valid currency rate and return it or None.
+        :param currencies: str
+        :return: str or None
+        """
         rates = self.bufferData.get(currencies)
         if rates and rates.get("time_next_update_unix") > time.time():
             return json.dumps(rates)
         return None
 
     def setFields(self, response):
+        """
+        Delete unnecessary fields and add error field to API response data.
+        :param response: str
+        :return: str
+        """
         response = json.loads(response)
         self._deleteUnnecessaryData(response)
         self._addErrorField(response)
@@ -89,70 +125,135 @@ class CurrencyServerFactory(ServerFactory):
 
     @staticmethod
     def _deleteUnnecessaryData(response):
+        """
+        Delete unnecessary fields from API response data.
+        :param response: dict
+        :return: dict
+        """
         del response["documentation"]
         del response["terms_of_use"]
         return response
 
     @staticmethod
     def _addErrorField(response):
+        """
+        Add error field to API response data.
+        :param response: dict
+        :return: dict
+        """
         response['error'] = None
         return response
 
 
 class HttpAgentHandler(object):
+    """
+    Class for handling http requests.
+    """
 
-    def __init__(self, agent, url):
+    def __init__(self, agent, url, bodyHandler):
+        """
+        Initialize HttpAgentHandler instance.
+        :param agent: twisted.web.client.Agent
+        :param url: str
+        :param bodyHandler: classobj
+        """
         self.agent = agent
-        self.url = url
+        self.url = url  # external API url
+        self.bodyHandler = bodyHandler  # class for handling response body
 
     def performRequest(self, data):
+        """
+        Send request to external API url.
+        :param data: str
+        :return: Deferred
+        """
         url = self.url + data + "/"
         response = self.agent.request(b"GET", url)
         return response.addCallback(self.handleResponse)
 
     def handleResponse(self, response):
+        """
+        Obtain response body.
+        :param response: twisted.web._newclient.Response
+        :return: Deferred
+        """
         finished = defer.Deferred()
-        response.deliverBody(HttpBodyHandler(finished, response.code, response.phrase))
+        response.deliverBody(self.bodyHandler(finished, response.code, response.phrase))
         return finished
 
 
 class HttpBodyHandler(Protocol):
+    """
+    Class for obtaining response body.
+    """
 
     body = b""
 
     def __init__(self, deferred, code, phrase):
+        """
+        Initialize HttpBodyHandler instance.
+        :param deferred: Deferred
+        :param code: int
+        :param phrase: str
+        """
         self.deferred = deferred
-        self.code = code
-        self.phrase = phrase
+        self.code = code  # response status code
+        self.phrase = phrase  # response status info
 
     def dataReceived(self, data):
         self.body += data
 
     def connectionLost(self, reason):
+        """
+        Fire deferred according to conditions.
+        :param reason: Failure
+        :return:None
+        """
         self.deferred.addBoth(self.cleanDeferred)
         if reason.check(ResponseDone) and self.code == 200:
             self.deferred.callback(self.body)
-        elif reason.check(ResponseDone):
+        elif reason.check(ResponseDone):  # if request didn't succeed add response code and status into Failure
             responseInfo = "{} {}".format(self.code, self.phrase)
             self.deferred.errback(Exception(responseInfo))
         else:
             self.deferred.errback(reason)
 
     def cleanDeferred(self, value):
+        """
+        Prevent reusing fired deferred.
+        :param value: None
+        :return: None
+        """
         if self.deferred is not None:
             self.deferred = None
 
 
 class BufferHandler(object):
+    """
+    Class for handling response body.
+    """
 
     def __init__(self, bufferFile):
+        """
+        Initialize BufferHandler instance.
+        :param bufferFile: str
+        """
         self._bufferFile = bufferFile
 
     def reWriteBufferFile(self, data):
+        """
+        Clear buffer file and write new data.
+        :param data: dict
+        :return: None
+        """
         with open(self._bufferFile, "w") as f:
             json.dump(data, f)
     
     def getBufferData(self):
+        """
+        Return buffered data from buffer file if exists.
+        :return: dict or None
+        """
         if self._bufferExists():
             with open(self._bufferFile, "r") as f:
                 bufferData = json.load(f)
@@ -160,14 +261,22 @@ class BufferHandler(object):
         return {}
     
     def _bufferExists(self):
+        """
+        Check if buffer file exists and not empty
+        :return: bool
+        """
         return os.path.isfile(self._bufferFile) and os.path.getsize(self._bufferFile) > 0
 
 
 def main():
+    """
+    Create factory and relative objects, create TCP listener and run reactor.
+    :return: None
+    """
     buf = BufferHandler(BUFFER_FILE)
     from twisted.internet import reactor
     agent = Agent(reactor)
-    httpAgentHandler = HttpAgentHandler(agent, EXTERNAL_SERVER_URL)
+    httpAgentHandler = HttpAgentHandler(agent, EXTERNAL_SERVER_URL, HttpBodyHandler)
     factory = CurrencyServerFactory(httpAgentHandler, buf)
     reactor.listenTCP(port=PORT, interface=HOST, factory=factory)
     reactor.run()
